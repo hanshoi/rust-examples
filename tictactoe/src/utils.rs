aeba59deab0d2739a630502f47260c2b2ad03e15
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2019-06-10 11:36:43 +0300

    extracted input creation to Input impl

--- tictactoe/src/utils.rs
+++ tictactoe/src/utils.rs
@@ -5,36 +5,48 @@ pub struct Input {
     pub column: usize,
 }
 
+impl Input {
+    fn create(col: char, row: char) -> Input {
+      Input{
+        column: match col {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            _ => panic!("Wrong letter"),
+        },
+        row: match row {
+            '1' => 0,
+            '2' => 1,
+            '3' => 2,
+            _ => panic!("Wrong letter"),
+        },
+      }  
+    }
+}
 
-pub fn get_input() -> Input {
+fn read_char_input() -> (char, char) {
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to readline");
     let raw = guess.trim();
     if raw.len() < 2 {
         panic!("Too short input: {}", raw);
     }
-    let input = Input{
-        column: match &raw[0 .. 1] {
-            "a" => 0,
-            "b" => 1,
-            "c" => 2,
-            _ => panic!("Wrong letter"),
-        },
-        row: match &raw[1 .. 2] {
-            "1" => 0,
-            "2" => 1,
-            "3" => 2,
-            _ => panic!("Wrong letter"),
-        },
-    };
-    input
+    let mut raw_chars = raw.chars();
+    (
+        raw_chars.next().unwrap(),
+        raw_chars.next().unwrap()
+    )
+}
+
+pub fn get_input() -> Input {
+    let (col_char, row_char) = read_char_input();
+    Input::create(col_char, row_char)
 }
 
 pub fn get_tic(turn: u8) -> char {
-    let mark = match turn % 2 {
+    match turn % 2 {
         1 => 'O',
         0 => 'X',
         _ => 'O',
-    };
-    mark
+    }
 }
