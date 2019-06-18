7564b37bbbc7cdd4c332d43e15034c14ec6ebb48
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2019-06-18 17:24:46 +0300

    changed to use actix 1.0 and not 0.7

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -1,25 +1,22 @@
-extern crate actix_web;
-use actix_web::{server, App, HttpRequest, Responder, Path};
+// extern crate actix_web;
+use actix_web::{HttpServer, App, Responder, web};
 
-fn index(_req: &HttpRequest) -> &'static str {
+fn index() -> impl Responder {
     "Hello world!"
 }
 
-fn name(name: Path<String>) -> impl Responder {
+fn name(name: web::Path<String>) -> impl Responder {
     format!("Hello {}!", name)
 }
 
-fn main() {
-    let server = server::new(|| {
-        vec![
-            App::new()
-                .prefix("/hello")
-                .resource("/{name}", |r| r.with(name)),
-            App::new().resource("/", |r| r.f(index)),
-        ]
-    });
+fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(
+        || App::new()
+            .route("/hello/{name}", web::get().to(name))
+            .route("/", web::get().to(index))
+            .route("/index.html", web::get().to(index))
+    );
 
-    server.bind("127.0.0.1:8088")
-        .unwrap()
-        .run();
+    println!("listening to 127.0.0.1:8088");
+    server.bind("127.0.0.1:8088")?.run()
 }
