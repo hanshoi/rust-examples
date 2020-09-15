6b8df6b43d41a355cf8dcc9eff7aec5541064ed4
hanshoi <antti.ruotsalainen@hanshoi.net>
2020-09-15 20:00:47 +0300

    rust based git cd finished

--- cd_project/src/main.rs
+++ cd_project/src/main.rs
@@ -3,21 +3,25 @@ use std::env;
 use std::io::{self, Write};
 extern crate dirs;
 
+fn write(path: &[u8]){
+    io::stdout().write_all(path).unwrap_or(());
+}
+
 fn main() -> io::Result<()> {
-    let mut path = env::current_dir().unwrap();
-    let home = dirs::home_dir().unwrap();
-    let home_str = home.to_str().unwrap();
+    let mut path = env::current_dir()?;
+    let home_str = dirs::home_dir().map(
+        |p| p.as_path().display().to_string()
+    ).unwrap_or(String::from("/"));
 
     loop {
         if path.join(".git").is_dir() {
-            io::stdout().write_all(path.to_str().unwrap().as_bytes())?;
+            write(path.to_str().unwrap().as_bytes());
             break
         };
         path = match path.parent() {
             Some(path) => path.to_path_buf(),
             None => {
-                io::stdout().write_all(b".")?;
-                return Ok(())
+                break;
             },
         };
         if path.to_str().unwrap() == home_str {
