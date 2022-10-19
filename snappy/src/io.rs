73774f66c4befaeceda412c3c7fbeef21af1efa3
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2022-10-19 20:29:36 +0300

    simple snappy that saves previous command and executes said command next time you run it

--- snappy/src/io.rs
+++ snappy/src/io.rs
@@ -1,8 +1,9 @@
+use std::env;
 use std::fs::File;
-use std::io::{Error, Read};
+use std::io::{Error, Read, Write};
 use std::path::Path;
 
-pub fn read_as_bytes(path: impl AsRef<Path>) -> Result<Vec<u8>, Error> {
+pub fn read_home_dir(path: impl AsRef<Path>) -> Result<Vec<u8>, Error> {
     /* `path` is relative to the home directory */
     let home = dirs::home_dir().unwrap();
     let target = home.join(path);
@@ -14,4 +15,25 @@ pub fn read_as_bytes(path: impl AsRef<Path>) -> Result<Vec<u8>, Error> {
     }
 
     Ok(buffer)
+}
+
+pub fn read_current_dir(path: impl AsRef<Path>) -> Result<Vec<u8>, Error> {
+    let target = env::current_dir()?.join(path);
+    let mut buffer = Vec::new();
+
+    if target.exists() {
+        let mut file = File::open(target)?;
+        file.read_to_end(&mut buffer)?;
+    }
+
+    Ok(buffer)
+}
+
+pub fn write_current_dir(path: impl AsRef<Path>, content: &[u8]) -> Result<(), Error> {
+    let target = env::current_dir()?.join(path);
+
+    let mut file = File::create(target)?;
+    file.write_all(content)?;
+
+    Ok(())
 }
--- snappy/src/main.rs
+++ snappy/src/main.rs
@@ -4,27 +4,31 @@ use std::process::Command;
 mod history;
 mod io;
 
+static SNAPPY_FILE: &str = "snaps";
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 struct Cli {
-    #[arg(short, long)]
-    save: Option<String>,
+    #[arg(short, long, default_value_t = false)]
+    save: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
-    match cli.save {
-        None => {}
-        Some(_) => {}
+    if cli.save {
+        println!("Save!");
+        let last_command = history::get_last_command();
+        println!("{last_command}");
+        io::write_current_dir(SNAPPY_FILE, last_command.as_bytes()).unwrap();
+    } else {
+        let last_command = String::from_utf8(io::read_current_dir(SNAPPY_FILE).unwrap()).unwrap();
+
+        let command: Vec<&str> = last_command.split(' ').collect();
+
+        Command::new(command[0])
+            .args(&command[1..])
+            .status()
+            .expect("process failed to execute");
     }
-    let last_command = history::get_last_command();
-
-    println!("{last_command}");
-    let command: Vec<&str> = last_command.split(' ').collect();
-
-    Command::new(command[0])
-        .args(&command[1..])
-        .status()
-        .expect("process failed to execute");
 }
